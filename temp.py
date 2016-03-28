c8d438bf5e998a176970e23cbe5a70a84f7439b3
Vishakh Kumar <vkumar@gatech.com>
2016-03-28 18:46:19 -0400

    Final project set up

--- temp.py
+++ temp.py
@@ -3,17 +3,59 @@ from sys import argv
 summ = ""
 
 f = open('text1.txt', 'r')
-g = open('result1.txt','w')
+g = open('chapterwise.txt','w')
+
+word = [' and ',' or ',' goes ',' her ',' him ',' he ',' she ',' the ',' from ',' a ' ,' an ','SHOT',"MOVING","BEDROOM","EXT"]
+
+chapterList = [str(i) for i in range(0,303)]
+
+charList = ["FINCH","SCOUT","JEM","JEAN Louise","REYNOLD","STEPHANIE","MAUDIE"]
+
+
+
+
+
+
 
 for line in f:
-   line = line.rstrip('\n')
-   line = line + " "
-   line.replace("\0", " ")
-   summ = summ + line
+    line = line.replace('.'," ")
+    line = line.rstrip('\n')
+    line = line + " "
+    for i in word:
+        if i in line:
+           line = line.replace(i," ")
+    for i in chapterList:
+        if i in line:
+           line = line.replace(i,'",\n"' )
 
-print summ
-
-g.write(summ)
-
-f.close()
+    line = line.replace('"",',"")
+    g.write(line)
+    g.write("\n")
 g.close()
+f.close()
+
+
+
+
+
+f = open('text1.txt', 'r')
+g = open('characterwise.txt','w')
+
+
+for line in f:
+    line = line.replace('.'," ")
+    line = line.rstrip('\n')
+    line = line + " "
+    for i in word:
+        if i in line:
+           line = line.replace(i," ")
+    for i in charList:
+        if i in line:
+           line = line.replace(i,'",\n\n"' )
+
+    line = line.replace('"",',"")
+    line = line.replace('.'," ")
+    g.write(line)
+    g.write("\n")
+g.close()
+f.close()
